b9790f07f0d2c6e5a8b8d66d95402b5d7d7e4e8f
Nikhil <60028530+nickel-yadav@users.noreply.github.com>
2021-08-18 12:54:36 +0530

    :construction: Add routing, models and controllers

--- app.js
+++ app.js
@@ -3,24 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var mongoose = require('mongoose');
 
 var app = express();
 
-// Connecting to the database 
-const { client } =  require('pg');
-const connectionString = 'postgres://postgres:postgres@localhost:5432/test';
-const client = new Client({
-  connectionString: connectionString
-});
+// Connect the mongodb database
+const uri = "mongodb+srv://mitnick:gHyoPJeTtmdk1zay@sandbox.u7ruw.mongodb.net/Library?retryWrites=true&w=majority";
+// Mongoose connecting to the mongoDB database
+mongoose.connect(uri, {  
+  useNewUrlParser: true,  
+  useUnifiedTopology: true}).then(() => { console.log('MongoDB Connected')}).catch(err => console.log(err));
 
-client.connect();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -30,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -47,4 +48,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(8080);
 module.exports = app;
